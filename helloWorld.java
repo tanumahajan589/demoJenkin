33b9ae33e54006c35b63f93a16f450bb9da91ef4
tanumahajan589 <tanumahajan589@gmail.com>
2024-08-21 23:33:20 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,5 @@
 class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Saksham!
-                           Abi btati hu photos baby"); 
+        System.out.println("Hello, Saksham! Abi btati hu photos baby"); 
     }
 }
